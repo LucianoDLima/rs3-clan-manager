90fb2eacc34411f75ce9a5099dc9400b1a14a885
Luciano Lima <ljr.luciano.junior@outlook.com>
2026-03-11 13:33:21 -0300

    better handle the error in the activity sync

--- src/services/memberSyncNull.ts
+++ src/services/memberSyncNull.ts
@@ -15,25 +15,40 @@ export async function syncMissingLastOnline(clanId: number) {
     try {
       const url = `https://apps.runescape.com/runemetrics/profile/profile?user=${encodeURIComponent(member.name)}`;
       const res = await fetch(url);
+
+      if (!res.ok) {
+        console.warn(`${clanId} - ${member.name} - Skip: Server returned ${res.status}`);
+        continue;
+      }
+
+      const contentType = res.headers.get('content-type');
+      if (!contentType || !contentType.includes('application/json')) {
+        console.warn(
+          `${clanId} - ${member.name} - Skip: Received HTML instead of JSON (API Lag, it shouldn't happen)`,
+        );
+        continue;
+      }
+
       const data = (await res.json()) as RuneMetricsResponse;
 
       if (data.error === 'PROFILE_PRIVATE') {
         // TODO: Add a row in the db to mark it as private maybe?
-        console.log(`${member.name} - Skipped: Private Profile.`);
+        console.log(`${clanId} - ${member.name} - Skipped: Private Profile.`);
         continue;
       }
 
       if (data.activities?.length) {
         const firstActivityDate = new Date(data.activities[0].date);
-
         await updateLastActivity(member.id, firstActivityDate);
-
-        console.log(`${member.name} - Updated: ${firstActivityDate.toISOString()}`);
+        console.log(`${clanId} - ${member.name} - Updated: ${firstActivityDate.toISOString()}`);
       }
     } catch (error) {
-      console.error(`${member.name} - error at ${new Date().toISOString()}:`, error);
+      console.error(`${clanId} - ${member.name} - error:`, error);
     }
 
-    await new Promise((resolve) => setTimeout(resolve, 1500));
+    // to avoid 429 rate limit error
+    await new Promise((resolve) => setTimeout(resolve, 2000));
   }
+
+  console.log(`Done for ${clanId}`);
 }
